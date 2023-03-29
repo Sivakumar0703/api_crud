66dae169ef490cb24951932ad17d40a29a12276c
sivakumar0703 <ssivakumar358@gmail.com>
2023-03-29 16:18:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import TeacherProfile from './teacher/teacherProfile';
+import StudentProfile from './student/studentProfile';
+import AddStudent from './student/addStudent';
+import Dashboard from './components/dashboard';
+import AddTeacher from './teacher/addTeacher';
+import NoPage from './components/noPage';
+import ViewStudent from './student/viewStudent';
+import ViewTeacher from './teacher/viewTeacher';
+import EditStudentData from './student/editStudent';
+import EditTeacherData from './teacher/editTeacher';
+
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Switch>
+
+        <Route exact path='/'>
+          <Dashboard />
+        </Route>
+
+        <Route path='/student&profile'>
+          <StudentProfile />
+        </Route>
+
+        <Route path='/teacher&profile'>
+
+          <TeacherProfile />
+
+        </Route>
+
+        <Route path='/add&student'>
+          <AddStudent />
+        </Route>
+
+        <Route path='/add&teacher'>
+          <AddTeacher />
+        </Route>
+
+        <Route path='/viewStudent&:id'>
+          <ViewStudent />
+        </Route>
+
+        <Route path='/viewTeacher&:id'>
+          <ViewTeacher />
+        </Route>
+
+        <Route path='/editStudent:id'>
+          <EditStudentData />
+        </Route>
+
+        <Route path='/editTeacher:id'>
+          <EditTeacherData />
+        </Route>
+
+        <Route path='**'>
+          <NoPage />
+        </Route>
+
+      </Switch>
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom';
+import StudentProvider from './context/studentProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Router>
+    <StudentProvider>
     <App />
-  </React.StrictMode>
+    </StudentProvider>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
